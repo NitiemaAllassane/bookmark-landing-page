05bdae761fc2c373c3701ad5223e2cdfe61f52cf
Nitiema Allassane <allassanenitiema@gmail.com>
2025-06-26 01:19:18 +0000

    add: attribution message

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
     import DownloadSection from "./lib/DownloadSection.svelte";
     import FaqSection from "./lib/FaqSection.svelte";
     import Features from "./lib/Features.svelte";
-  import Footer from "./lib/Footer.svelte";
+    import Footer from "./lib/Footer.svelte";
     import Header from "./lib/Header.svelte";
     import Hero from "./lib/Hero.svelte";
     import Newsletter from "./lib/Newsletter.svelte";
@@ -17,4 +17,11 @@
     <FaqSection />
     <Newsletter />
     <Footer />
+    <small class=" inline-block p-2 text-center w-full bg-white text-blue-950">
+        Challenge by 
+        <a class=" text-blue-600 underline" href="https://www.frontendmentor.io/home" target="_blank">
+            Frontend Mentor
+        </a>.
+        Coded by <a class="text-blue-600 underline" href="https://www.frontendmentor.io/profile/NitiemaAllassane" target="_blank">Nitiema Allassane</a>.
+    </small>
 </main>
